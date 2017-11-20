74caf9ba09a721d3824a6b21f3e821ccd49237e7
alketh <Alexander.Keth@uni-hamburg.de>
2017-11-20 16:12:22 +0100

    Draft: Extract rating per episode.

--- R/ratings.R
+++ R/ratings.R
@@ -1,5 +1,31 @@
-#' Get series meta information from IMDB.
+#' Get episode specific series rating.
 
-ratings <- function() {
+id <- "0944947"
+season <- 3
 
+ratings <- function(id, season) {
+  url <- paste0("http://www.imdb.com/title/tt", id, "/episodes?season=", season, "&ref_=tt_eps_sn_", season)
+
+  html_raw <- xml2::read_html(url)
+
+  # Create links to episodes
+  episode_urls <- rvest::html_nodes(html_raw, "div")
+  episode_urls <- rvest::html_attr(episode_urls, "data-const")
+  episode_urls <- episode_urls[!is.na(episode_urls)]
+
+  # Read in raw rating html per episode.
+  rating_urls <- paste0("http://www.imdb.com/title/", episode_urls, "/ratings?ref_=tt_ov_rt")
+  ratings_raw <- purrr::map(rating_urls, xml2::read_html)
+
+  # Extract ratings and votes per age and gender ratings
+  rarings <- purrr::map(ratings_raw, ~rvest::html_table(.)[2][[1]])
+
+  rating <- ratings[[1]]
+  clean_ratings <- function(rating) {
+
+  }
+
+  # Extract episode names
+  names <- purrr::map(ratings_raw, ~rvest::html_nodes(., ".subnav_heading")) %>%
+    purrr::map_chr(., rvest::html_text)
 }
